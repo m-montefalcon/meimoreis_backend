f8225da9aa9fbafe7ac7b2b7a467ac7486c934e5
m-montefalcon <mmontefalconschlprps@gmail.com>
2024-05-27 21:24:47 +0800

    Added routes for user endpoint

--- index.js
+++ index.js
@@ -2,12 +2,24 @@
 import express from "express";
 import db from "./config/db.js";
 import userRoutes from "./routes/user.js"
+import bodyParser from "body-parser";
+import cors from "cors";
 
 const app = express();
+
 const port = 8000;
+//Cors Origin 
+app.use(cors());
+
+// Middleware to log request body
+app.use(express.json()); // Parse JSON bodies
+app.use(express.urlencoded({ extended: true })); // Parse URL-encoded bodies
+
+
 db.connect();
 app.get('/', (req, res) => {
-    res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Origin', '*');
+
     res.sendStatus(200);
 })
 
--- routes/user.js
+++ routes/user.js
@@ -1,10 +1,18 @@
-import express from "express";
+import express, { request } from "express";
+import morgan from "morgan";
+import multer from "multer"; // Import multer middleware
 const router = express.Router();
 
-router.get('/', (req, res) => {
-    res.send('Welcome to the homepage!');
-  });
+// Set up multer middleware to handle multipart/form-data
+const upload = multer();
 
+router.post('/register', upload.single(), (req, res) => {
+    // No need to set CORS headers here, they should be set globally
 
+    // Access the parsed request body data
+    const requestBodyData = req.body;
+    console.log(requestBodyData)
+    res.send(requestBodyData).status(200); // Send a 200 OK response with the request body data
+});
 
 export default router;
